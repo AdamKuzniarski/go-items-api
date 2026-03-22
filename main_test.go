a274128fe951678783e0344411ac3dea9c4688e4
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-03-22 15:07:38 +0100

    add setupRouter func and TestHealth

--- main_test.go
+++ main_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupTestRouter() *gin.Engine {
+	gin.SetMode(gin.TestMode)
+	return setupRouter(NewStore())
+}
+
+func TestHealth(t *testing.T) {
+	router := setupTestRouter()
+
+	req := httptest.NewRequest(http.MethodGet, "/health", nil)
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("expected 200, got %d", w.Code)
+	}
+}
